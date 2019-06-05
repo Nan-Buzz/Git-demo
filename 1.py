c7c2cba82c8c0dce059f85a3533777b067a8ee39
Nan-Buzz <ynxiazj@yeah.net>
2019-06-05 14:38:32 +0800

    jui

--- 1.py
+++ 1.py
@@ -5,3 +5,5 @@ b = 5
 c = self.assert_(boolean expression, 'message')
 for yu in gt:
     print("Hello world!!!")
+
+# This is yue xin ichg! 
--- xyn.py
+++ xyn.py
@@ -2,9 +2,6 @@ import os
 
 iu = 90
 lp = [3,4,5,6]
-for i in lp:
-    print(i)
-    rank(45)
 
 print("Hello world!")
 # This is change on dev and master
